3cda8b2fdc1cdbb7420ffd0b766ad535e58467fa
ricardobarcelosdonascimento <ricardobarcelos@terra.com.br>
2021-07-23 18:18:25 -0300

    Aula 23 07 2021

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-alert("Olá mundo")
+var fields = document.querySelectorAll('#form-user-create [name]');
+var user = {}
+
+document.getElementById('form-user-create').addEventListener('submit', event => {
+
+    event.preventDefault();
+
+    fields.forEach((field, index) => {
+
+        if (field.name === 'gender') {
+    
+            if (field.checked){
+    
+                user[field.name] = field.value;
+    
+            }     
+        }else {
+    
+            user[field.name] = field.value;
+
+        }
+    
+    });
+
+    console.log(user)
+
+})
+
